f41e0d3b01bb1344b631525a830a016e24cec1ec
ClaraSerr <clara.serruau4@etu.univ-lorraine.fr>
2022-04-24 22:59:02 -0400

    final

--- exploration.a2995765.js
+++ exploration.a2995765.js
@@ -329,7 +329,8 @@ var graphSize; /// /////////////////////////////////////////////////////////////
 
 function drawHeatmap(vizData, ligne, girouette) {
   //d3.select("#heatmap-svg").selectAll().remove()
-  document.getElementById("heatmap-svg-container").firstElementChild.innerHTML = '';
+  document.getElementById("heatmap-svg-container").innerHTML = '';
+  d3.select("#heatmap-svg-container").append("svg").attr('id', "heatmap-svg");
   HEIGHT = document.getElementById('heatmap-svg-container').getBoundingClientRect().height;
   var indicateur = document.getElementById("indicateur").value;
   var moyenne, liste, nomIndicateur;
@@ -9618,7 +9619,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51055" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "56802" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
